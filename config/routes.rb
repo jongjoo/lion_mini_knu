b94a31ec48e36ac2e626441a26feb28714711fe6
jeongjeahyoun <zeni9@naver.com>
2017-05-21 14:33:45 +0000

    777

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
   get 'pages/index'
+  
+  post 'pages/mail_ok' => 'pages#mail_ok'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
